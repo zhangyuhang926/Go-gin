f10f3dbb734ffcdef4689a19afc57d32191d3df4
zhangyuhang926 <1907599510@qq.com>
2020-06-02 18:17:08 +0800

    zhangyuhang926

---
+++ gin_form/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+// 获取form表单提交的参数
+func main(){
+	r := gin.Default()
+	r.LoadHTMLFiles("./login.html", "./index.html")
+	r.GET("/login", func(c *gin.Context){
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+	r.POST("/login", func (c *gin.Context)  {
+		// 获取form表单提交的数据
+		// username := c.PostForm("username")
+		// password := c.PostForm("password") // 取到就返回值，起不到就返回一个空字符串
+		// username := c.DefaultPostForm("username", "somebody")
+		// password := c.DefaultPostForm("password", "****")
+
+		username, ok := c.GetPostForm("username")
+		if !ok {
+			username = "somebody"
+		}
+		password, ok := c.GetPostForm("password")
+		if !ok {
+			password = "****"
+		}
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Name": username,
+			"Password": password,
+		})
+	})
+	r.Run(":9090")
+}
